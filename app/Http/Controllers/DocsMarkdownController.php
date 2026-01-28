62107411e0d167e2eaa2dc0701f18cf613970c4a
Duncan McClean <duncan@duncanmcclean.com>
2026-01-28 18:24:40 +0000

    Append `.md` to internal links

--- app/Http/Controllers/DocsMarkdownController.php
+++ app/Http/Controllers/DocsMarkdownController.php
@@ -22,8 +22,41 @@ class DocsMarkdownController extends Controller
             ])->filter()->implode("\n\n");
         });
 
+        $markdown = $this->appendMdExtensionToInternalLinks($markdown);
+
         return response($markdown, 200, [
             'Content-Type' => 'text/markdown; charset=UTF-8',
         ]);
+    }
+
+    private function appendMdExtensionToInternalLinks(string $markdown): string
+    {
+        return preg_replace_callback(
+            '/(?<!!)\[([^\]]+)\]\(([^)]+)\)/',
+            function ($matches) {
+                $text = $matches[1];
+                $url = $matches[2];
+
+                if ($this->shouldAppendMdExtension($url)) {
+                    $url .= '.md';
+                }
+
+                return "[$text]($url)";
+            },
+            $markdown
+        );
+    }
+
+    private function shouldAppendMdExtension(string $url): bool
+    {
+        if (preg_match('/^https?:\/\//', $url)) {
+            return false;
+        }
+
+        if (preg_match('/\.[a-z0-9]{2,4}$/i', $url)) {
+            return false;
+        }
+
+        return true;
     }
 }
